1265e07fb238ba9ef413921f1b238e87e9376707
Stacy <146508027+TyuninaA@users.noreply.github.com>
2024-05-13 18:47:46 +0300

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
+
+const App = ({ Component, pageProps }) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
+export default App;
