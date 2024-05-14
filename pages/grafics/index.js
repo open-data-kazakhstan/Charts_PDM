b57d5166cb5a768c4d5513c3bda52c3396837fdb
Stacy <146508027+TyuninaA@users.noreply.github.com>
2024-05-14 10:31:36 +0300

    Update index.js

--- pages/grafics/index.js
+++ pages/grafics/index.js
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
+
 const Grafics = () => (
-    <h1>Здесь будет список всех графиков.</h1>
-  );
-  
-  export default Grafics;
+  <div>
+    <h1 style={{ color: "#181E35" }}>Вы можете ознакомиться со следующими графиками:</h1>
+    <div className="buttonContainer">
+      <Link href="/grafics/kz_population" passHref>
+        <button className="button">KZ Population</button>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Grafics;
